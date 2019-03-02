1e8a75e4fabd875d10c9e9b2e0ab4f1cea79897c
stoyangarov <stoyan.garov@yahoo.com>
2019-03-02 21:27:14 +0000

    Heroku config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send("Hi");
+	res.send({ name: "Hi" });
 });
 
 const PORT = process.env.PORT || 5000;

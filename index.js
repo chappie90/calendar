a535b5f649b089407b97096f51ce22fc8ab4b76f
stoyangarov <stoyan.garov@yahoo.com>
2019-03-03 15:39:08 +0000

    Set up heroku deployment

--- index.js
+++ index.js
@@ -12,5 +12,16 @@ app.use(bodyParser.json());
 
 require("./routes/reminderRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+	// Express will serve production assets, e.g. main.js or main.css
+	app.use(express.static("client/build"));
+
+	// Express will serve index.html file
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
